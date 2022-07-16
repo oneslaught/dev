76e53785f620ae1bcc246e8f68b17b1cd4fb6bc3
oneslaught <iceminecraft021@gmail.com>
2022-07-16 13:54:44 +0300

    sum of 2 numbers

--- main.go
+++ main.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+    fmt.Print("Enter a number: ")
+    var number1 float64
+    fmt.Scanf("%f", &number1)
 
+	fmt.Print("Enter a second number: ")
+    var number2 float64
+    fmt.Scanf("%f", &number2)
+
+    output := number1 + number2
+
+    fmt.Println(number1,"+", number2, "=", output)    
 }
